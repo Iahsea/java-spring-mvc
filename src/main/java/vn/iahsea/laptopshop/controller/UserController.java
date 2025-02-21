f8c40afafc90672ebe9431b37febefb37c00dd33
Iahsea <ngyenchinhhai@gmail.com>
2025-02-21 17:15:06 +0700

    007 chapter 7

--- src/main/java/vn/iahsea/laptopshop/controller/UserController.java
+++ src/main/java/vn/iahsea/laptopshop/controller/UserController.java
@@ -1,12 +1,16 @@
 package vn.iahsea.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.iahsea.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -14,10 +18,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 // @RestController
 
 public class UserController {
+
+    private UserService userService;
+
     
-    @RequestMapping("/")
-    public String getHomePage(){
-        return "eric.html";
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @RequestMapping("/")  
+    public String getHomePage(Model model){
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("iahsea", "This is Iahsea");
+        return "hello";
     }
     
 }
