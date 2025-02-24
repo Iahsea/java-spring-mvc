75fd579b30cb85570ed8588d87b425dd0ef7ae7c
Iahsea <ngyenchinhhai@gmail.com>
2025-02-24 23:44:17 +0700

    008 chapter 11

--- src/main/java/vn/iahsea/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/iahsea/laptopshop/controller/admin/UserController.java
@@ -1,5 +1,9 @@
 package vn.iahsea.laptopshop.controller.admin;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.servlet.ServletContext;
 import vn.iahsea.laptopshop.domain.User;
 import vn.iahsea.laptopshop.repository.UserRepository;
 import vn.iahsea.laptopshop.service.UserService;
@@ -20,70 +26,87 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
-
-
 @Controller
 // @RestController
 
 public class UserController {
 
     private final UserService userService;
+    private final ServletContext servletContext;
 
-    
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ServletContext servletContext) {
         this.userService = userService;
+        this.servletContext = servletContext;
     }
 
-
-    @RequestMapping("/")  
-    public String getHomePage(Model model){
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
         List<User> arrUsers = this.userService.getAllUsersByEmail("iah@gmail.com");
         System.out.println(arrUsers);
         model.addAttribute("eric", "hi");
         model.addAttribute("iahsea", "This is Iahsea");
         return "hello";
     }
-    
-    @RequestMapping("/admin/user")  
-    public String getUserPage(Model model){
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("users1", users);
         return "admin/user/show";
     }
 
-    @RequestMapping("/admin/user/{id}")  
-    public String getUserDetailPage(Model model, @PathVariable long id){
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         model.addAttribute("id", id);
         return "admin/user/detail";
     }
 
-    @RequestMapping("/admin/user/create")  
-    public String getCreateUserPage(Model model){
+    @GetMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "admin/user/create", method = RequestMethod.POST)  
-    public String createUserPage(Model model, @ModelAttribute("newUser") User iahsea){
-        this.userService.handleSaveUser(iahsea);
+    @PostMapping(value = "admin/user/create")
+    public String createUserPage(Model model,
+            @ModelAttribute("newUser") User iahsea,
+            @RequestParam("iahseaFile") MultipartFile file) {
+        
+        try {
+            byte[] bytes = file.getBytes();
+
+            String rootPath = this.servletContext.getRealPath("/resources/images");
+            File dir = new File(rootPath + File.separator + "avatar");
+            if (!dir.exists())
+                dir.mkdirs();
+            // Create the file on server
+            File serverFile = new File(dir.getAbsolutePath() + File.separator +
+                    +System.currentTimeMillis() + "-" + file.getOriginalFilename());
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // this.userService.handleSaveUser(iahsea);
         return "redirect:/admin/user";
     }
-    
-    @RequestMapping("/admin/user/update/{id}")  
-    public String getUpdateUserPage(Model model, @PathVariable long id){
+
+    @RequestMapping("/admin/user/update/{id}")
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
         User currentUser = this.userService.getUserById(id);
         model.addAttribute("newUser", currentUser);
         return "admin/user/update";
     }
 
     @PostMapping("/admin/user/update")
-    public String postUpdateUser(Model model, @ModelAttribute("newUser") User iahsea){
+    public String postUpdateUser(Model model, @ModelAttribute("newUser") User iahsea) {
         User currentUser = this.userService.getUserById(iahsea.getId());
-        if(currentUser != null){
+        if (currentUser != null) {
             currentUser.setAddress(iahsea.getAddress());
             currentUser.setFullName(iahsea.getFullName());
             currentUser.setPhone(iahsea.getPhone());
@@ -92,39 +115,36 @@ public class UserController {
         }
         return "redirect:/admin/user";
     }
-    
+
     @GetMapping("/admin/user/delete/{id}")
-    public String getDeleteUserPage(Model model, @PathVariable long id){
+    public String getDeleteUserPage(Model model, @PathVariable long id) {
         model.addAttribute("id", id);
         // User user = new User();
         // user.setId(id);
         model.addAttribute("newUser", new User());
         return "admin/user/delete";
     }
-    
+
     @PostMapping("/admin/user/delete")
-    public String postDeleteUser(Model model, @ModelAttribute("newUser") User iahsea){
+    public String postDeleteUser(Model model, @ModelAttribute("newUser") User iahsea) {
         this.userService.deleteAUser(iahsea.getId());
         return "redirect:/admin/user";
     }
-    
-}
 
+}
 
 // public class UserController {
 
-    // private UserService userService;
+// private UserService userService;
 
-    // public UserController(UserService userService) {
-    //     this.userService = userService;
-    // }
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
 
+// @GetMapping("")
+// public String getHomePage(){
+// return this.userService.handleHello();
+// // return "hello";
+// }
 
-    // @GetMapping("")
-    // public String getHomePage(){
-    //     return this.userService.handleHello();
-    //     // return "hello";
-    // }
-    
-    
 // }
