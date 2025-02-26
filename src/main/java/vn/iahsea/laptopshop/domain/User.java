d50fe4c0ef5bd0066e495d87f9be295cf2b96380
Iahsea <ngyenchinhhai@gmail.com>
2025-02-26 18:10:47 +0700

    008 chapter 12

--- src/main/java/vn/iahsea/laptopshop/domain/User.java
+++ src/main/java/vn/iahsea/laptopshop/domain/User.java
@@ -12,6 +12,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -24,22 +25,22 @@ public class User {
     private Long id;
 
     @NotNull
-    @Email
+    @Email(message = "Email không hợp lệ", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
 
     @NotNull
-    @Min(2)
+    @Size(min = 2, message = "Password phải có tối thiểu 2 ký tự")
     private String password;
 
     @NotNull
-    @Min(2)
+    @Size(min = 3, message = "Fullname phải có tối thiểu 3 ký tự")
     private String fullName;
 
     private String address;
 
     private String phone;
 
-    private String avatar; 
+    private String avatar;
 
     // roleId
 
@@ -50,115 +51,77 @@ public class User {
     @OneToMany(mappedBy = "user")
     List<Order> orders;
 
-    
-    
     public Role getRole() {
         return role;
     }
-
-
 
     public void setRole(Role role) {
         this.role = role;
     }
 
-
-
     public List<Order> getOrders() {
         return orders;
     }
-
-
 
     public void setOrders(List<Order> orders) {
         this.orders = orders;
     }
 
-
-
     public Long getId() {
         return id;
     }
-
-
 
     public void setId(Long id) {
         this.id = id;
     }
 
-
-
     public String getEmail() {
         return email;
     }
-
-
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-
-
     public String getPassword() {
         return password;
     }
-
-
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-
-
     public String getFullName() {
         return fullName;
     }
-
-
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
-
-
     public String getAddress() {
         return address;
     }
-
-
 
     public void setAddress(String address) {
         this.address = address;
     }
 
-
-
     public String getPhone() {
         return phone;
     }
-
-
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
-
-
     public String getAvatar() {
         return avatar;
     }
 
-
-
     public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
-
-
 
     @Override
     public String toString() {
@@ -166,6 +129,4 @@ public class User {
                 + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
-    
-    
 }
