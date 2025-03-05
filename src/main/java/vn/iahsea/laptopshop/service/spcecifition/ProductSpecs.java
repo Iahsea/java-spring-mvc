f604890476c89c6ebbfc6d049180dcb5a1e0550c
Iahsea <ngyenchinhhai@gmail.com>
2025-03-05 14:34:46 +0700

    008 152 chapter 16

--- src/main/java/vn/iahsea/laptopshop/service/spcecifition/ProductSpecs.java
+++ src/main/java/vn/iahsea/laptopshop/service/spcecifition/ProductSpecs.java
@@ -34,4 +34,9 @@ public class ProductSpecs {
             criteriaBuilder.le(root.get(Product_.PRICE), max)
         ); 
     }
+
+    public static Specification<Product> matchMultiplePrice(double min, double max){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.between(
+            root.get(Product_.PRICE), min, max);
+    }
 }
