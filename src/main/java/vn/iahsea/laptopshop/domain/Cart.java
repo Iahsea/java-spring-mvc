5cecc4a71746227ae6ee7f7ea805bca3ee17ae09
Iahsea <ngyenchinhhai@gmail.com>
2025-03-09 15:45:31 +0700

    010 166 chapter 17

--- src/main/java/vn/iahsea/laptopshop/domain/Cart.java
+++ src/main/java/vn/iahsea/laptopshop/domain/Cart.java
@@ -17,6 +17,8 @@ import jakarta.validation.constraints.Min;
 @Table(name = "carts")
 public class Cart implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
--- src/main/java/vn/iahsea/laptopshop/domain/CartDetail.java
+++ src/main/java/vn/iahsea/laptopshop/domain/CartDetail.java
@@ -14,6 +14,8 @@ import jakarta.persistence.Table;
 @Table(name = "cart_detail")
 public class CartDetail implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
